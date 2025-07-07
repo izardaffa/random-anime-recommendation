72c45490c0ae9cabc891b413f0c21750962e6873
Nizar Daffa Maulana <izardev@gmail.com>
2025-07-08 00:25:44 +0700

    feat: add background image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,15 @@
 function Main({ children }) {
   return (
-    <div className={`flex justify-center items-center min-h-screen py-28`}>
-      {children}
+    <div 
+      className={`relative flex justify-center items-center min-h-screen py-28`}
+    >
+      <div 
+        className="absolute inset-0 opacity-30 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: "url('bg.png')" }}
+      ></div>
+      <div className="relative z-10">
+        {children}
+      </div>
     </div>
   );
 }
